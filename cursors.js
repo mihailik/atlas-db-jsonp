500e69e65087a6c4ca281a6fc7f7aaa3183cc158
Oleg Mihailik <mihailik@gmail.com>
2023-09-02 16:57:53 +0100

    Cloned from atlas-db

---
+++ cursors.js
@@ -0,0 +1,7 @@
+var cursors=(jsonp=>(typeof cursors==='function'?cursors(jsonp):0,cursors=cursors))({
+  "users": {
+    "timestamp": 1693427085385,
+    "oneByOne": "eswv73itwgpafxgnozr4nncu",
+    "listRepos": "1693073825422::did:plc:qqtlqswztt2hnsaijk7u7hmc"
+  }
+})
